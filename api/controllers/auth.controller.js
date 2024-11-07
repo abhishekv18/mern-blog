014f6f3c79ec746b094faf5591c633606d4b1e2a
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2024-11-07 22:45:55 +0530

    add a middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
     const { username, email, password } = req.body;
     if(!username||!email||!password||username==''||email==''||password==''){
-        return res.status(400).json({message:'all fields are required'});
+        next(errorHandler(400,'all fields are required'));
 
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -14,7 +15,7 @@ export const signup=async(req,res)=>{
       await newUser.save();
       res.json('signup succesfull');
      } catch (error) {
-      res.stsus(500).json({message:error.message});
+    next(error);
      }
     
 };
