dbe5704f3649b8d289ddb268e6cc92e3ae6546f8
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2024-11-30 23:35:48 +0530

    user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,11 +3,13 @@ import {
    
   test,
   updateUser,
+  deleteUser,
+  
   } from '../controllers/user.controller.js';
   import { verfiyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test',test);
 router.put('/update/:userId',verfiyToken,updateUser);
-
+router.delete('/delete/:userId',verfiyToken,deleteUser);
 export default router;
