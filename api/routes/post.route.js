fa955ee7d8dfe30b2251d2d1d5b24489b60da3f1
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2024-12-01 19:39:17 +0530

    create a post api route

---
+++ api/routes/post.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { verfiyToken } from '../utils/verifyUser.js';
+import { create } from '../controllers/post.controller.js';
+const router = express.Router();
+
+
+router.post('/create',verfiyToken,create);
+
+export default router;
