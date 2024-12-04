77f72ecc10d06043c46e7b357fd469d59b603645
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2024-12-04 15:36:55 +0530

    update post func

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { verfiyToken } from '../utils/verifyUser.js';
-import { create ,getposts,deletepost} from '../controllers/post.controller.js';
+import { create ,getposts,deletepost,updatepost} from '../controllers/post.controller.js';
 const router = express.Router();
 
 
 router.post('/create',verfiyToken,create);
 router.get('/getposts', getposts);
 router.delete('/deletepost/:postId/:userId', verfiyToken, deletepost);
-
+router.put('/updatepost/:postId/:userId', verfiyToken, updatepost)
 export default router;
